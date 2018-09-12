fa15c53ee36c6f7fc2b210ea1793cf27f0df0e13
Brannon Dorsey <brannon@brannondorsey.com>
2018-09-12 19:34:17 -0400

    Add MagicBytes to header.

--- src/CVPacket.ts
+++ src/CVPacket.ts
@@ -6,6 +6,8 @@ export enum HeaderFlags {
     Signed = 0x2
 }
 
+export const MagicBytes = [0x7a, 0x39]
+
 export interface Header {
     version: number,
     compressed: boolean,
@@ -68,7 +70,7 @@ export class Packet {
         if (this.header.compressed) flags = flags | HeaderFlags.Compressed
         
         // header buffer
-        const headerArray = [this.header.version, flags]
+        const headerArray = [...MagicBytes, this.header.version, flags]
         if (this.signature !== null) {
             if (this.signature.length > 256) throw Error('signature is larger than 256 bytes')
             headerArray.push(this.signature.length)
@@ -82,25 +84,29 @@ export class Packet {
     async disassemble(data: Buffer): Promise<void> {
 
         if (!Buffer.isBuffer(data)) throw TypeError('data must be a Buffer')
-        if (Buffer.length < 3) throw Error('invalid packet length')
 
-        const version = data[0]
+        const magic = data.slice(0, 2)
+        if (magic[0] !== MagicBytes[0] || magic[1] !== MagicBytes[1]) {
+            throw Error(`Invalid magic bytes in packet header. This is not a CV Packet.`)
+        }
+
+        const version = data[2]
         if (version !== 1) {
-            throw Error(`invalid packet version: ${version}`)
+            throw Error(`Invalid packet version: ${version}`)
         }
 
         // COME BACK HERE
-        const flags = data[1]
+        const flags = data[3]
         this.header.compressed = (flags & HeaderFlags.Compressed) == HeaderFlags.Compressed
         this.header.signed = (flags & HeaderFlags.Signed) == HeaderFlags.Signed
 
         // console.log(`compressed: ${this.header.compressed}`)
         // console.log(`signed: ${this.header.signed}`)
 
-        let payloadIndex = 2
+        let payloadIndex = 4
         if (this.header.signed) {
-            this.header.signatureLength = data[2]
-            payloadIndex = 3
+            this.header.signatureLength = data[4]
+            payloadIndex = 5
         }
 
         let payload: Buffer = data.slice(payloadIndex)
@@ -116,7 +122,7 @@ export class Packet {
             // console.log(`signature length: ${this.header.signatureLength}`)
             // console.log(`payload length: ${payload.length}`)
         }
-        
+
 
         this.message = payload.slice(messageIndex).toString('utf8')
     }
@@ -124,7 +130,7 @@ export class Packet {
     static async ToAX25Packet(fromCallsign: string, 
                               toCallsign: string, 
                               utf8Text: string, 
-                              signature: Buffer): Promise<any> {
+                              signature?: Buffer): Promise<any> {
         
         const packet = new Packet()
         packet.from = fromCallsign
@@ -152,8 +158,8 @@ export class Packet {
         }
 
         const packet = new Packet()
-        packet.from = ax25Packet.source.callsign
-        packet.to = ax25Packet.destination.callsign
+        packet.from = ax25Packet.source.callsign.trim()
+        packet.to = ax25Packet.destination.callsign.trim()
         await packet.disassemble(ax25Packet.payload)
         return packet
     }
