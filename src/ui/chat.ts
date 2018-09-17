6077f7105a013a82d846ba59d55dd8b6c48292d9
Brannon Dorsey <brannon@brannondorsey.com>
2018-09-17 18:25:56 -0400

    Add color login in ui/chat!

--- src/ui/chat.ts
+++ src/ui/chat.ts
@@ -3,22 +3,53 @@ import { terminal as term } from 'terminal-kit'
 import { Messenger, MessageEvent, Verification } from '../Messenger';
 
 term.on('key', (name: string , matches: string[], data: any): void => {
-    name; // intentionally unused
-    data; // intentionally unused
     if ( matches.includes('CTRL_C') || matches.includes('CTRL_D')) {
-        term.processExit(0)
+        exit(0)
     }
 })
 
-export function begin(): void {
+type TerminalFunction = (buffer: string) => TerminalFunction // function
+const colorMap: { [callsign: string]: TerminalFunction } = {}
+const myColorFunction: TerminalFunction = term.cyan
+
+function getColorFunction(callsign: string): TerminalFunction {
+    
+    const choices = [
+        term.red,
+        term.green,
+        term.yellow,
+        term.blue,
+        term.magenta,
+        term.brightRed,
+        term.brightGreen,
+        term.brightYellow,
+        term.brightBlue,
+        term.brightMagenta
+    ]
+
+    // for (let color of choices) {
+    //     color('How does this color look?')
+    // }
+
+    if (!colorMap.hasOwnProperty(callsign)) {
+        colorMap[callsign] = choices[Math.floor(Math.random() * choices.length)]
+    }
+
+    return colorMap[callsign]
+}
+
+export function enter(): void {
     term.fullscreen()
+}
+
+export function exit(code: number): void {
+    term.processExit(code)
 }
 
 export async function printReceivedMessage(message: MessageEvent, callsign: string): Promise<void> {
 
     const pos: { x: number, y: number } = await term.getCursorLocation()
     if (pos) {
-
         term.moveTo(0, pos.y)
         term.insertLine(1)
         printStyledText(message)
@@ -28,7 +59,7 @@ export async function printReceivedMessage(message: MessageEvent, callsign: stri
             if (input) {
                 // term.moveTo(0, term.height)
                 term.moveTo(0, term.height)
-                term.green(`${callsign}`)(': ')
+                myColorFunction(`${callsign}`)(': ')
                 input.redraw()
             }
         }
@@ -38,17 +69,19 @@ export async function printReceivedMessage(message: MessageEvent, callsign: stri
 export async function inputLoop(callsign: string, messenger: Messenger, sign: boolean): Promise<void> {
     while (true) {
         const text = (await prompt(callsign)).trim()
+        // TODO dynamic to addresses, not just CQ. "TOCALL: message" should only
+        // be sent appear to specific user.
         if (text !== '') await messenger.send('CQ', text, sign)
         term.eraseLine()
         const pos: { x: number, y: number } = await term.getCursorLocation()
         term.moveTo(0, pos.y)
-        term.green(`${callsign}`)(`: ${text}`)
+        myColorFunction(`${callsign}`)(`: ${text}`)
     }
 }
 
 let input: any
 async function prompt(callsign: string): Promise<string> {
-    term.green(`\n${callsign}`)(': ')
+    myColorFunction(`\n${callsign}`)(': ')
     input = term.inputField({ cancelable: true })
     const message = await input.promise
     return message
@@ -64,7 +97,7 @@ function printStyledText(message: MessageEvent): void {
     // else if (rand < 0.75) message.verification = Verification.Invalid
     // else message.verification = Verification.KeyNotFound
 
-    term.cyan(`${message.from}`)    
+    getColorFunction(message.from)(`${message.from}`)    
     switch (message.verification) {
         
         case Verification.NotSigned:
