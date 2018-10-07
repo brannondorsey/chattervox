329e916d3b21b6405ef4cb5a19018e98ebdd9367
Brannon Dorsey <brannon@brannondorsey.com>
2018-10-07 16:22:22 -0400

    Fix showkey bug and identify signing key in showkey output.

--- src/subcommands/showkey.ts
+++ src/subcommands/showkey.ts
@@ -4,18 +4,25 @@ import { Keystore, Key } from '../Keystore'
 export async function main(args: any, conf: Config, ks: Keystore): Promise<number> {
 
     if (args.callsign) {
-        printKeys(args.callsign.toUpperCase(), ks)
+        printKeys(args.callsign.toUpperCase(), ks, conf)
     } else {
-        ks.getCallsigns().forEach((callsign) => printKeys(callsign, ks))
+        ks.getCallsigns().forEach((callsign) => printKeys(callsign, ks, conf))
     }
 
     return 0
 }
 
-function printKeys(callsign: string, keystore: Keystore): void {
-    const keys: Key[] = keystore.getKeyPairs(callsign)
+function printKeys(callsign: string, keystore: Keystore, conf: Config): void {
+    
+    const keys: Key[] = keystore.getKeys(callsign)
     keys.forEach((key) => {
-        if (key.public) console.log(`${callsign} Public Key: ${key.public}`)
+        if (key.public) {
+            if (key.public === conf.signingKey) {
+                console.log(`${callsign} Public Key (your signing key): ${key.public}`)
+            } else {
+                console.log(`${callsign} Public Key: ${key.public}`)
+            }
+        }
         if (key.private) console.log(`${callsign} Private Key: ${key.private}`)
     })
 }
