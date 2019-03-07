442c420117f83aa116332c770fbe306054275d23
Brannon Dorsey <brannon@brannondorsey.com>
2019-03-06 21:50:43 -0500

    Add packaging support for MacOS.

--- pkg.sh
+++ pkg.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
+OS="$(uname -s)"
 
 rm -rf ./pkg 
 
-# linux x64
-OUTPUT_DIR=chattervox-linux-x64
-pkg --out-path "pkg/${OUTPUT_DIR}" \
-    --targets node8-linux-x64 . && \
-    cp node_modules/serialport/build/Release/serialport.node "pkg/${OUTPUT_DIR}"
+if [ "$(expr substr $OS 1 5)" == "Linux" ]; then
+    echo "Building for Linux..."
 
-pushd pkg
-tar -zcvf "${OUTPUT_DIR}.tar.gz" "${OUTPUT_DIR}"
-popd
+    # linux x64
+    OUTPUT_DIR=chattervox-linux-x64
+    pkg --out-path "pkg/${OUTPUT_DIR}" \
+        --targets node8-linux-x64 . && \
+        cp node_modules/serialport/build/Release/serialport.node "pkg/${OUTPUT_DIR}"
 
-# linux x86
-OUTPUT_DIR=chattervox-linux-x86
-pkg --out-path "pkg/${OUTPUT_DIR}" \
-    --targets node8-linux-x86 . && \
-    cp node_modules/serialport/build/Release/serialport.node "pkg/${OUTPUT_DIR}"
+    pushd pkg
+    tar -zcvf "${OUTPUT_DIR}.tar.gz" "${OUTPUT_DIR}"
+    popd
 
-pushd pkg
-tar -zcvf "${OUTPUT_DIR}.tar.gz" "${OUTPUT_DIR}"
-popd
+    # linux x86
+    OUTPUT_DIR=chattervox-linux-x86
+    pkg --out-path "pkg/${OUTPUT_DIR}" \
+        --targets node8-linux-x86 . && \
+        cp node_modules/serialport/build/Release/serialport.node "pkg/${OUTPUT_DIR}"
 
+    pushd pkg
+    tar -zcvf "${OUTPUT_DIR}.tar.gz" "${OUTPUT_DIR}"
+    popd
+elif [ "$OS" == "Darwin" ]; then
+    echo "Building for MacOS..."
+
+    # MacOS
+    OUTPUT_DIR=chattervox-macos
+    pkg --out-path "pkg/${OUTPUT_DIR}" \
+        --targets node8-macos-x64 . && \
+        cp node_modules/serialport/build/Release/serialport.node "pkg/${OUTPUT_DIR}"
+
+    pushd pkg
+    tar -zcvf "${OUTPUT_DIR}.tar.gz" "${OUTPUT_DIR}"
+    popd
+else
+    echo "unsupported OS $OS, exiting."
+fi
